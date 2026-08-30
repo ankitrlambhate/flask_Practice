2587561925dea1c6bcffc929c7f40e133c234469
ankitrlambhate <160218129+ankitrlambhate@users.noreply.github.com>
2026-08-30 21:41:12 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,18 +5,15 @@ from bson.objectid import ObjectId
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/test_student_db"  # test DB
+
+    # Reinitialize Mongo with the current configuration
+    mongo.init_app(app)
+
     client = app.test_client()
 
-    # Setup: clear and create test data
     with app.app_context():
         mongo.db.students.delete_many({})
-        mongo.db.students.insert_one({
-            "_id": ObjectId("66fddff25f4b5f6a0a123456"),
-            "name": "Test Student",
-            "email": "test@student.com",
-            "course": "Flask"
-        })
+
     yield client
 
     # Teardown: drop DB after test
